539ba70d662c5717dcba6d5e2c16f379d28452be
popaalex1 <popaalex5@icloud.com>
2025-06-30 13:49:49 +0300

    code formatting

--- diabetes_prediction.py
+++ diabetes_prediction.py
@@ -1,9 +1,9 @@
 import pandas as pd
+import xgboost as xgb
 from pathlib import Path
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score, classification_report, confusion_matrix
 from imblearn.over_sampling import SMOTE
-import xgboost as xgb
 
 file_path = Path.home() / "Documents" / "Proiect" / "dataset" / "diabetes_health_indicators.csv"
 df = pd.read_csv(file_path)
@@ -12,7 +12,6 @@ df = pd.read_csv(file_path)
 df_0 = df[df.Diabetes_012 == 0].sample(80000, random_state=42, replace=False)
 df_1 = df[df.Diabetes_012 == 1]
 df_2 = df[df.Diabetes_012 == 2]
-
 
 df_combined = pd.concat([df_0, df_1, df_2])
 
@@ -42,10 +41,8 @@ best_sampling_strategy = {
     2: 79980
 }
 
-
 smote = SMOTE(sampling_strategy=best_sampling_strategy, random_state=42)
 X_res, y_res = smote.fit_resample(X, y)
-
 
 X_train_valid, X_test, y_train_valid, y_test = train_test_split(
     X_res, y_res, test_size=0.2, stratify=y_res, random_state=42
@@ -54,19 +51,18 @@ X_train, X_valid, y_train, y_valid = train_test_split(
     X_train_valid, y_train_valid, test_size=0.1, stratify=y_train_valid, random_state=42
 )
 
-
 model = xgb.XGBClassifier(**best_params)
 model.fit(X_train, y_train)
 
 #validation metrics
 y_pred_valid = model.predict(X_valid)
-print("\nValidation Set Evaluation:")
+print("\nValidation Set Results:")
 print(f"F1 Macro: {f1_score(y_valid, y_pred_valid, average='macro'):.4f}")
 print(f"Accuracy: {accuracy_score(y_valid, y_pred_valid):.4f}")
 
 #test metrics
 y_pred_test = model.predict(X_test)
-print("\nTest Set Evaluation:")
+print("\nTest Set Results:")
 print(f"F1 Macro: {f1_score(y_test, y_pred_test, average='macro'):.4f}")
 print(f"Accuracy: {accuracy_score(y_test, y_pred_test):.4f}")
 
