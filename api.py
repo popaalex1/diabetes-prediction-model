6ed7e5fa807af4ab50ad4af155ff42fadb678db1
popaalex1 <popaalex5@icloud.com>
2025-07-01 13:46:09 +0300

    api release

--- api.py
+++ api.py
@@ -5,28 +5,28 @@ import xgboost as xgb
 import joblib
 
 class DiabetesInput(BaseModel):
-    Diabetes_012: int
-    HighBP: int
-    HighChol: int
-    CholCheck: int
+    Diabetes_012: float
+    HighBP: float
+    HighChol: float
+    CholCheck: float
     BMI: float
-    Smoker: int
-    Stroke: int
-    HeartDiseaseorAttack: int
-    PhysActivity: int
-    Fruits: int
-    Veggies: int
-    HvyAlcoholConsump: int
-    AnyHealthcare: int
-    NoDocbcCost: int
-    GenHlth: int
-    MentHlth: int
-    PhysHlth: int
-    DiffWalk: int
-    Sex: int
-    Age: int
-    Education: int
-    Income: int
+    Smoker: float
+    Stroke: float
+    HeartDiseaseorAttack: float
+    PhysActivity: float
+    Fruits: float
+    Veggies: float
+    HvyAlcoholConsump: float
+    AnyHealthcare: float
+    NoDocbcCost: float
+    GenHlth: float
+    MentHlth: float
+    PhysHlth: float
+    DiffWalk: float
+    Sex: float
+    Age: float
+    Education: float
+    Income: float
 
 app = FastAPI()
 model = joblib.load("diabetes_model.joblib")
